a443f904df66bef7e9f88ae2a9e1cbcad69b3eaa
sven-so <svenso@web.de>
2022-12-03 20:35:58 +0100

    added calmly integration

---
+++ scripts/content.js
@@ -0,0 +1,39 @@
+const insert = (content) => {
+  //const element = document.querySelector('[data-lexical-text="true"]');
+  const elements = document.getElementsByClassName('droid');
+
+  if (elements.length === 0) {
+    return;
+  }
+  
+  const element = elements[0];
+  const pToRemove = element.childNodes[0];
+  pToRemove.remove();
+  const splitContent = content.split('\n');
+  splitContent.forEach((content) => {
+    const p = document.createElement('p');
+  
+    if (content === '') {
+      const br = document.createElement('br');
+      p.appendChild(br);
+    } else {
+      p.textContent = content;
+    }
+  
+    // Insert into HTML one at a time
+    element.appendChild(p);
+  });
+  return true;
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.message === 'inject') {
+      const { content } = request;
+      const result = insert(content);
+      console.log(content);
+      if (!result) {
+        sendResponse({ status: 'failed' });
+      }
+      sendResponse({ status: 'success' });
+    }
+  });
